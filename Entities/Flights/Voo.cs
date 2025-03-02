69cf4b08350415085cbeaa648f84482fef63c1d7
Maurício Dionísio de Souza <dionmauricio@gmail.com>
2025-03-02 09:34:01 -0300

    feat: Cria a entidade Voo

---
+++ Entities/Flights/Voo.cs
@@ -0,0 +1,33 @@
+using CiaAerea.Entities.Aircraft;
+using CiaAerea.Entities.Pilots;
+
+namespace CiaAerea.Entities.Flights;
+
+public class Voo
+{
+    public Voo(
+        string origem,
+        string destino,
+        DateTime dataHoraPartida,
+        DateTime dataHoraChegada,
+        int aeronaveId,
+        int pilotoId)
+    {
+        Origem = origem;
+        Destino = destino;
+        DataHoraPartida = dataHoraPartida;
+        DataHoraChegada = dataHoraChegada;
+        AeronaveId = aeronaveId;
+        PilotoId = pilotoId;
+    }
+
+    public int Id { get; set; }
+    public string Origem { get; set; }
+    public string Destino { get; set; }
+    public DateTime DataHoraPartida { get; set; }
+    public DateTime DataHoraChegada { get; set; }
+    public int AeronaveId { get; set; }
+    public int PilotoId { get; set; }
+    public Aeronave Aeronave { get; set; } = null!;
+    public Piloto Piloto { get; set; } = null!;
+}
