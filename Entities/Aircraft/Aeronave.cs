bc59dcfc3503c8af94ad30ec191403465afa0a7e
Maurício Dionísio de Souza <dionmauricio@gmail.com>
2025-03-02 09:24:06 -0300

    feat: Cria entidade aeronave

---
+++ Entities/Aircraft/Aeronave.cs
@@ -0,0 +1,16 @@
+namespace CiaAerea.Entities.Aircraft;
+
+public class Aeronave
+{
+    public Aeronave(string fabricante, string modelo, string codigo)
+    {
+        Fabricante = fabricante;
+        Modelo = modelo;
+        Codigo = codigo;
+    }
+
+    public int Id { get; set; }
+    public string Fabricante { get; set; }
+    public string Modelo { get; set; }
+    public string Codigo { get; set; }
+}
