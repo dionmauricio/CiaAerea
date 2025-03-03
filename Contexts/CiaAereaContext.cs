80648b1c3c273aa7579d2c2e1ef5567e53e9a542
Maurício Dionísio de Souza <dionmauricio@gmail.com>
2025-03-03 11:04:27 -0300

    feat: Configura a conexão com banco de dados

--- Contexts/CiaAereaContext.cs
+++ Contexts/CiaAereaContext.cs
@@ -9,9 +9,23 @@ namespace CiaAerea.Contexts;
 
 public class CiaAereaContext : DbContext
 {
+    private readonly IConfiguration _configuration;
+
+    public CiaAereaContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
     public DbSet<Aeronave> Aeronaves => Set<Aeronave>();
     public DbSet<Piloto> Pilotos => Set<Piloto>();
     public DbSet<Voo> Voos => Set<Voo>();
     public DbSet<Cancelamento> cancelamentos => Set<Cancelamento>();
     public DbSet<Manutencao> Manutencoes => Set<Manutencao>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_configuration.GetConnectionString("CiaAerea"));
+
+        base.OnConfiguring(optionsBuilder);
+    }
 }
